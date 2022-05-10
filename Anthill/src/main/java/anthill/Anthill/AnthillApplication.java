bbf8929b2fee25f044d334251051e1df94177d22
Jisue <49144667+Jisue@users.noreply.github.com>
2022-05-10 22:38:50 +0900

    settings

---
+++ Anthill/src/main/java/anthill/Anthill/AnthillApplication.java
@@ -0,0 +1,13 @@
+package anthill.Anthill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnthillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnthillApplication.class, args);
+	}
+
+}
---
+++ Anthill/src/test/java/anthill/Anthill/AnthillApplicationTests.java
@@ -0,0 +1,13 @@
+package anthill.Anthill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnthillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
